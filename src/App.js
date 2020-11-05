21f2ddd4c395f9561ef9dc320d6feaed3a0c6f26
KaladaJumbo <wkalada1@gmail.com>
2020-11-05 12:34:37 -0500

    hooks init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = (props) => {
+
+ const [currentUser, setCurrentUser] = useState(null)
+ const [allChannels, setAllChannels] = useState([])
+ const [currentChannel, setCurrentChannel] = useState({channel: {}, users: [], messages: []})
+
+  // const openConnection = () => {
+  //   return new WebSocket('wss://localhost:3000/cable')
+  // }
+
   return (
     <h1> Hello world! </h1>
   );
