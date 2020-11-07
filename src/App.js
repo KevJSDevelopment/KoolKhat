0356d04478d0b92924ea7d47ff4647cbcde2db12
KevJSDevelopment <Kshhn12@gmail.com>
2020-11-07 16:38:13 -0500

    messages working

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ const App = (props) => {
       </Typography>
 
       {/* <NavBar/> */} 
-   
+      
       <Container className= {classes.container} maxWidth= 'xl'>
         <Grid container>
           {/* left side  */}
